37fd021b8f881c3cae968428a4c11de04405652c
davey <davey89451@gmail.com>
2021-06-20 11:34:10 -0700

    Express Generator REST API

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promtionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
+
 
 var app = express();
 
@@ -21,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promtionRouter);
+app.use('/partners', partnerRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
